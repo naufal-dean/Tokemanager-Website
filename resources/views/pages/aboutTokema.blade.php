34d3c0d1ee7879c53bf65ba34fc9a5b7684a486a
naufal-dean <naufal.dean@gmail.com>
2019-08-10 02:30:56 +0700

    Update About page.

--- resources/views/pages/aboutTokema.blade.php
+++ resources/views/pages/aboutTokema.blade.php
@@ -2,20 +2,42 @@
 
 @section('content')
 <main role="main" class="container">
-  <div class="overflow-auto">
-    <h1>FITUR-FITUR TOKEMANAGER</h1>
+  <h1 class="mt-5 mb-5">Tentang TOKEMANAGER</h1>
+
+  <div class="overflow-auto jumbotron">
     <h2>1. Penjualan</h2>
-      <p>Tab <strong>Penjualan</strong> berfungsi untuk mencatat transaksi yang sedang dilakukan.</p>
+      <div class="ml-4">
+        <p class="mb-0">Tab <strong>Penjualan</strong> berfungsi untuk mencatat transaksi yang sedang dilakukan.</p>
+        <small>Catatan: Perlu diimplementasikan fitur autofill, sehingga tidak perlu mengisi harga secara manual untuk mencegah kesalahan.</small>
+      </div>
+
     <h2>2. Keuangan</h3>
-      <h3 style="font-size:20px"> a. Statistik</h3>
-      <p>Tab <strong>Statistik Keuangan</strong> berfungsi melihat statistik keuangan.</p>
-      <h3 style="font-size:20px"> b. Timeline</h3>
-      <p>Tab <strong>Timeline Keuangan</strong> berfungsi melihat timeline penjualan tiap harinya.</p>
+      <div class="ml-4">
+        <h3 style="font-size:20px"> a. Statistik</h3>
+        <div class="ml-3">
+          <p class="mb-0">Tab <strong>Statistik Keuangan</strong> berfungsi melihat statistik keuangan.</p>
+          <small>Catatan: Belum diimplementasikan.</small>
+        </div>
+
+        <h3 style="font-size:20px"> b. Timeline</h3>
+        <div class="ml-3">
+          <p>Tab <strong>Timeline Keuangan</strong> berfungsi melihat timeline penjualan tiap harinya.</p>
+        </div>
+      </div>
+
     <h2>3. Barang</h2>
-    <h3 style="font-size:20px"> a. Statistik Barang</h3>
-    <p>Tab <strong>Statistik Barang</strong> berfungsi melihat statistik barang yang terjual.</p>
-    <h3 style="font-size:20px"> a. Database Barang</h3>
-    <p>Tab <strong>Database Barang</strong> berfungsi melihat barang-barang yang tersedia.</p>
+      <div class="ml-4">
+        <h3 style="font-size:20px"> a. Statistik Barang</h3>
+        <div class="ml-3">
+          <p class="mb-0">Tab <strong>Statistik Barang</strong> berfungsi melihat statistik barang yang terjual.</p>
+          <small>Catatan: Belum diimplementasikan.</small>
+        </div>
+
+        <h3 style="font-size:20px"> a. Database Barang</h3>
+        <div class="ml-3">
+          <p>Tab <strong>Database Barang</strong> berfungsi melihat barang-barang yang tersedia.</p>
+        </div>
+      </div>
   </div>
 </main>
 @endsection
